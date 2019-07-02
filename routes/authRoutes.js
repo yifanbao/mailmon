899eedacb3b74bfbe9e6be73d94f86c3b676a9c4
yifann <yifanbao@outlook.com>
2019-07-02 22:27:44 +0800

    add github auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,25 @@
-const passport = require('passport');
+const passport = require("passport");
 
 module.exports = app => {
   app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
-}
+  app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get(
+    "/auth/github",
+    passport.authenticate("github", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  app.get("/auth/github/callback", passport.authenticate("github"));
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,52 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys')
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      // console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: keys.githubClientID,
+      clientSecret: keys.githubClientSecret,
+      callbackURL: "/auth/github/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOrCreate({ githubId: profile.id }).then(() => {
+        return done(null, user);
+      });
     }
   )
 );
