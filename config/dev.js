36f08ab856be8448c48b3ec2653b9035a3114268
yifann <yifanbao@outlook.com>
2019-07-06 21:54:48 +0800

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: '532761557737-nrq5srjsneg2atj0gd8cgqhe10cghh7r.apps.googleusercontent.com',
+  googleClientSecret: 'MgF6ej2WYV27Vdlttmp9Kapo',
+  githubClientID: 'aafd910edc387d894f68',
+  githubClientSecret: 'b59355274edd3144ae8034be9182299b0d063863',
+  mongoURI: 'mongodb+srv://byfan:TouBw4yUvaHFYpaR@mailmon-iqr7s.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'sdlkfjlsnnclskdjnoisfjrklflksndflkasnlwejfdsljflksdjf'
+}
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport')
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true }, err => {
+  console.log(err);
+});
 
 const app = express();
 
@@ -19,7 +21,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
