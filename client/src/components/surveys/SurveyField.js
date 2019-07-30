81d4bfe046c17cad7c91d5bc9c44b71fc2f3e478
yifann <yifanbao@outlook.com>
2019-07-30 17:19:57 +0800

    Implement SurveyNew page

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+// Contains logic to render a single label and text input
+import React from 'react';
+
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+// Shows SurveyForm and SurveyFormReview
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
