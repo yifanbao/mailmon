d85f5c54bc79830273485febdb8a9839d0029b04
yifann <yifanbao@outlook.com>
2019-07-30 18:40:23 +0800

    Implement SurveyForm validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title'},
@@ -39,6 +40,21 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) {
+  const errors= {};
+
+  errors.recipients = validateEmails(values.recipients || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = 'Please provide a value';
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
