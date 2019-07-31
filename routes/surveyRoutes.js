55deedaf674e633a02c0d5aaf21a710c4295c683
yifann <yifanbao@outlook.com>
2019-07-31 18:04:11 +0800

    Handle webhook data, update SendGrid API key

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,8 +10,38 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your feedback!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('surveyId', 'email')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $set: { 'recipients.$.responded': true },
+            $inc: { [choice]: 1 },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -26,13 +59,13 @@ module.exports = app => {
     const mailer = new Mailer(survey, surveyTemplate(survey));
 
     try {
-      // await mailer.send();
+      await mailer.send();
       await survey.save();
       req.user.credits -= 1;
       const user = await req.user.save();
 
       res.send(user);
-    } catch(err) {
+    } catch (err) {
       res.status(422).send(err);
     }
   });
